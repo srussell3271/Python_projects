0579dad43bde44e580c4204a0e9a3c63cba5d0cb
srussell3271 <33640724+srussell3271@users.noreply.github.com>
2018-08-02 15:34:59 -0400

    Create adventure.py

---
+++ adventure.py
@@ -0,0 +1,154 @@
+# print("S/O to Giselle and Danielle even though they had a rough time they got though it")
+
+start = '''
+You wake up one morning and find that you aren't in your bed; you aren't even in your room.
+You're in the middle of a nowhere.
+A sign is hanging from your neck: "You have one house and one door to open. Find the key.Don't look back."
+There is a house to your right and to your left.
+'''
+print(start)
+user_input1 = ''
+
+while user_input1 != 'left' and user_input1 != 'right':
+    print("Type 'left' to go left or 'right' to go right.") # Update to match your story.
+    user_input1 = input()
+    if user_input1 == "left":
+        print('''
+        You decide to go left and as you approach the house.
+        There was a sign in front the house it said
+        "Welcome to your Doom!" with blood dripping down on it
+        You thought of going in and you did
+        Some doors were cracked open and some where close
+        You was wondering if you go to the upstairs door or stay on this floor''')
+    print("Type 'upstairs' or 'stay there'")
+
+    user_input2 = input()
+
+    if user_input2 == "upstairs":
+        print('''Upstairs you go.
+                You see a bird who has a key strapped to a chain
+                The bird looks at you and flies out the window
+                It head straight to the fancy house on the otherside
+                You sigh hard and rolled your eyes
+                Another bird flies in and sits at the window
+                It hass been an hours time to die.''')
+        print("A voice: yOu lOse. TiMe to dIe!")
+        break
+    elif user_input2 == "stay":
+        print(''' There is a sign telling you
+        'Try another place. This is the wrong room
+        you die
+        ''')
+        print("A voice: yOu lOse. TiMe to dIe!")
+        break
+        # Continue code to finish story.
+
+    elif user_input1 == "right":
+        print('''You choose to go right and you see a nice fancy house.
+            You wonder if you shoud take a peak and go inside
+            You see two doors on opposite end
+            The doors at the end of hallway next to each other
+            One is on the left the other is on the right
+            ''')
+    print("Type 'left' or 'right'")
+
+    user_input3 = input()
+
+    if user_input3 == "left":
+        print('''A bird flies in with a key strapped on its neck.
+        It flies to you and you take the key off
+        Another bird flies in with a note in its wing
+        You take the note and it reads''')
+        print('''LeAvE tHe HoUSe and lOck ThE DoOr
+        SoMeOne WiLl Come gEt yOu anD TakE yOu HomE. You WIN''')
+        print('''You leave the house quickly and lock the doors
+        Some came and carried you to a motorcyle and took you into the night
+        You reach your house and it was decorated with glitter saying
+        YOU MADE IT''')
+        break
+    elif user_input3 == "right":
+        print(''' You pushed the door but it was locked
+        There was a bunch of rattling inside then it got quiet and opened up
+        A guy with a butcher knife and hit you in your head''')
+        print("YOU DIE!")
+        break
+        # Update to match your story.
+
+        # Continue code to finish story.
+# print("S/O to Giselle and Danielle even though they had a rough time they got though it")
+
+start = '''
+You wake up one morning and find that you aren't in your bed; you aren't even in your room.
+You're in the middle of a nowhere.
+A sign is hanging from your neck: "You have one house and one door to open. Find the key.Don't look back."
+There is a house to your right and to your left.
+'''
+print(start)
+user_input1 = ''
+
+while user_input1 != 'left' and user_input1 != 'right':
+    print("Type 'left' to go left or 'right' to go right.") # Update to match your story.
+    user_input1 = input()
+    if user_input1 == "left":
+        print('''
+        You decide to go left and as you approach the house.
+        There was a sign in front the house it said
+        "Welcome to your Doom!" with blood dripping down on it
+        You thought of going in and you did
+        Some doors were cracked open and some where close
+        You was wondering if you go to the upstairs door or stay on this floor''')
+    print("Type 'upstairs' or 'stay there'")
+
+    user_input2 = input()
+
+    if user_input2 == "upstairs":
+        print('''Upstairs you go.
+                You see a bird who has a key strapped to a chain
+                The bird looks at you and flies out the window
+                It head straight to the fancy house on the otherside
+                You sigh hard and rolled your eyes
+                Another bird flies in and sits at the window
+                It hass been an hours time to die.''')
+        print("A voice: yOu lOse. TiMe to dIe!")
+        break
+    elif user_input2 == "stay":
+        print(''' There is a sign telling you
+        'Try another place. This is the wrong room
+        you die
+        ''')
+        print("A voice: yOu lOse. TiMe to dIe!")
+        break
+        # Continue code to finish story.
+
+    elif user_input1 == "right":
+        print('''You choose to go right and you see a nice fancy house.
+            You wonder if you shoud take a peak and go inside
+            You see two doors on opposite end
+            The doors at the end of hallway next to each other
+            One is on the left the other is on the right
+            ''')
+    print("Type 'left' or 'right'")
+
+    user_input3 = input()
+
+    if user_input3 == "left":
+        print('''A bird flies in with a key strapped on its neck.
+        It flies to you and you take the key off
+        Another bird flies in with a note in its wing
+        You take the note and it reads''')
+        print('''LeAvE tHe HoUSe and lOck ThE DoOr
+        SoMeOne WiLl Come gEt yOu anD TakE yOu HomE. You WIN''')
+        print('''You leave the house quickly and lock the doors
+        Some came and carried you to a motorcyle and took you into the night
+        You reach your house and it was decorated with glitter saying
+        YOU MADE IT''')
+        break
+    elif user_input3 == "right":
+        print(''' You pushed the door but it was locked
+        There was a bunch of rattling inside then it got quiet and opened up
+        A guy with a butcher knife and hit you in your head''')
+        print("YOU DIE!")
+        break
+        # Update to match your story.
+
+        # Continue code to finish story.
